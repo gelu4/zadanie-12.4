21d62e686f65522fcdea66ef002f8e4eb1922097
Grzegorz Koza <kozagrzegorz@interia.pl>
2018-01-18 00:45:41 +0100

    Add error

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ var paragraph = document.getElementById('joke');
 function getJoke() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url);
+  xhr.addEventListener("error", function() {
+    alert("BŁĄD");
+  });
   xhr.addEventListener('load', function(){
     var response = JSON.parse(xhr.response);
     paragraph.innerHTML = response.value.joke;
